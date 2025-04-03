66adf6ca7165c74354aac40b1ff5e0a6445026cb
Galia Almaraz <galia.almaraz@uab.edu.bo>
2025-04-03 16:22:51 -0400

    hola mundo

--- hola.py
+++ hola.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "¡Hola, Flask está funcionando!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+print('hola mundo')
